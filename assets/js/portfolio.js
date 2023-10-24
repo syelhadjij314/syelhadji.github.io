f27700b4d8056565b548169b2f39ff6738777f05
EL HADJI SY <syelaj314@gmail.com>
2023-10-24 10:52:33 +0000

    add detail page

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -8,29 +8,30 @@ function showPortfolioDetails(itemId) {
     // Vous pouvez utiliser un objet, un tableau ou une autre source de données pour obtenir les détails appropriés
 
     // Exemple de données de détails du portefeuille
-        var portfolioData = {
+    var portfolioData = {
         title: "Exemple de projet " + itemId,
         category: "Web design",
         client: "Société XYZ",
         projectDate: "01 mars 2020",
         projectURL: "www.example.com",
         description: "Ceci est un exemple de détail de portefeuille pour l'élément " + itemId + ". Autem ipsum nam porro corporis rerum. Quis eos dolorem eos itaque inventore commodi labore quia quia. Exercitationem repudiandae officiis neque suscipit non officia eaque itaque enim. Voluptatem officia accusantium nesciunt est omnis tempora consectetur dignissimos. Sequi nulla at esse enim cum deserunt eius."
-        };
+    };
     
         // Mettez à jour le contenu du conteneur avec les détails du portefeuille
-        portfolioDetailsContainer.innerHTML = `
-        <h3>${portfolioData.title}</h3>
-        <ul>
-            <li><strong>Category</strong>: ${portfolioData.category}</li>
-            <li><strong>Client</strong>: ${portfolioData.client}</li>
-            <li><strong>Project date</strong>: ${portfolioData.projectDate}</li>
-            <li><strong>Project URL</strong>: <a href="${portfolioData.projectURL}">${portfolioData.projectURL}</a></li>
-        </ul>
-        <p>${portfolioData.description}</p>
-        `;
+    portfolioDetailsContainer.innerHTML = `
+    <h3>${portfolioData.title}</h3>
+    <ul>
+        <li><strong>Category</strong>: ${portfolioData.category}</li>
+        <li><strong>Client</strong>: ${portfolioData.client}</li>
+        <li><strong>Project date</strong>: ${portfolioData.projectDate}</li>
+        <li><strong>Project URL</strong>: <a href="${portfolioData.projectURL}">${portfolioData.projectURL}</a></li>
+    </ul>
+    <p>${portfolioData.description}</p>
+    `;
+    console.log(portfolioDetailsContainer);
+
     }
     console.log('okkkkkkkkkkk');
-    console.log(itemId);
 
 
   // Obtenez tous les boutons "bi bi-plus-circle" et ajoutez un gestionnaire d'événements de clic à chacun
